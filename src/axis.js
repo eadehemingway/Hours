4fb8dc89bfbedabfc40e4e2fd9babd3ec5a6d26e
Chris Newell <newechs@aol.com>
2022-01-24 00:09:07 +0100

    Add new texture blocks

--- src/axis.js
+++ src/axis.js
@@ -74,10 +74,10 @@ export function drawAxis(ctx, width, height, x, y) {
         console.warn("Axis not drawn");
         return;
     }
+    // ctx.translate(0.5, 0.5); // straddle lines
     let font = new FontFace('goudy-old-style', 'url(https://use.typekit.net/af/4d44c2/00000000000000007735a6eb/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3)');
     let start;
     const y_space_for_labels = 60;
-    const x_space_for_labels = 0;
     let axis_lines = width <= 400 ? lines.filter((d, i) => i % 2 == 0) : lines;
     axis_lines = axis_lines.map(d => ({ hour: d, completed: false }));
     const lines_count = (axis_lines.length - 1);
