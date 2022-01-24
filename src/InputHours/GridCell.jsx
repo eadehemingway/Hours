82182ff9cb2f2807c3e5d1c3abd00ff88b02e803
Chris Newell <newechs@aol.com>
2022-01-24 18:26:59 +0100

    Add initial textures to hour cells

--- src/InputHours/GridCell.jsx
+++ src/InputHours/GridCell.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import { fillHours, emptyHours, FILLED, UNFILLED } from "./hours";
 import styled from "styled-components";
+import grain_2 from '../wood_grain-02.jpg';
+import grain_3 from '../wood_grain-03.jpg';
+import grain_4 from '../wood_grain-04.jpg';
+import grain_5 from '../wood_grain-05.jpg';
+import grain_6 from '../wood_grain-06.jpg';
+import grain_7 from '../wood_grain-07.jpg';
+import grain_8 from '../wood_grain-08.jpg';
+import grain_9 from '../wood_grain-09.jpg';
+
+function getRandomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export  function GridCell({ cat, hours_index, startDragging, onDraggedOver, endDragging, isPending }) {
     const is_already_filled = cat[hours_index] === FILLED;
     const is_pending = isPending(cat.category, hours_index);
     return (
-        <Cell is_pending={is_pending} is_already_filled={is_already_filled}
+        <Cell is_pending={is_pending} is_already_filled={is_already_filled} texture={grain_1}
 
             onMouseDown={(e) => {
                 e.preventDefault();
@@ -26,9 +38,16 @@ export  function GridCell({ cat, hours_index, startDragging, onDraggedOver, endD
 const Cell = styled.div`
 border: 1px solid;
   height: 100%;
-  background: ${({ is_already_filled, is_pending })=>{
+  background-color: ${({ is_already_filled, is_pending })=>{
         if (is_pending) return "#E15838";
         if (is_already_filled && !is_pending) return "white";
         else return "#E0DACE";
-    }}
+    }};
+::before {
+    content: url("${ props => props.texture }");
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+}
 `;
